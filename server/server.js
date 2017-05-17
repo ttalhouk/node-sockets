510c26a1b0baa5eeeeed86c93b795b96338327df
Talal Talhouk <talal@sbcglobal.net>
2017-05-16 21:52:48 -0700

    add sockets and connection events

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000; // required for heroku deployment
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req,res) => {
-//   res.sendFile(publicPath + '/index.html');
-// })
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', (socket) => {
+    console.log('user disconnected');
+  });
+});
 
-app.listen(port, () =>{
+server.listen(port, () =>{
   console.log('Server running on port '+port);
 });
