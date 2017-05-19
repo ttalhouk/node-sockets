23b37923c87ea66f8051cf235ff9a0cec63ec240
Talal Talhouk <talal@sbcglobal.net>
2017-05-19 10:52:12 -0700

    add connection messages

--- server/server.js
+++ server/server.js
@@ -14,7 +14,19 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+// User connections
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New User has joined',
+      createdAt: new Date().getTime()
+    });
 
+// User messages
   socket.on('createMessage', (newMsg) => {
     console.log('Create Message', newMsg);
     io.emit('newMessage', {
@@ -22,7 +34,7 @@ io.on('connection', (socket) => {
       text: newMsg.text,
       createdAt: new Date().getTime()
     });
-  })
+  });
 
   socket.on('disconnect', (socket) => {
     console.log('user disconnected');
